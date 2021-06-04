6f8107f00614de3c3d95d6e3460793bf2bbd20f0
Minenik2 <39184444+Minenik2@users.noreply.github.com>
2021-06-04 18:54:31 +0200

    update 1.0
    made the code simpler and added comments for more clearance

--- kode.ino
+++ kode.ino
@@ -1,8 +1,38 @@
 #include <LiquidCrystal.h>
 #include <Servo.h>
 
-LiquidCrystal lcd(11, 12, 2, 3, 4, 5);
-// alt for å fikse poeng og motorer
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+#define echoPin 9
+#define trigPin 10
+
+char *utfordringer[] = {"Gjett 20cm avstand", "Skyt"};
+char *utfordringerLang[] = {"Gjett 20cm avstand",
+                                  "Hink lengre enn venstremann",
+                                  "Skyt eldste spiller på 10 meter",
+                                  "Sta pa henda i 10sek",
+                                  "Dobbelt poeng pa neste utfordring",
+                                  "+1 poeng til venstremann ved feil pa neste utfordring",
+                                  "Sta 1 meter unna sensor",
+                                  "Skyt i varet og fang skuddet",
+                                  "20 push-ups",
+                                  "30 squats",
+                                  "Gjor 1 pistol squat",
+                                  "Forste til a skyte med hoyrehand",
+                                  "Kast 3 terninger hver med hoyremann, den med høyest samlet sum vinner",
+                                  "Skyt ned en brikke fra 10 meter",
+                                  "Skyt spiller til venstre fra 5 meter imens han/hun dukker unna"
+                                 };
+int currentUtfordringIndex;
+char* aktuellUtfordring;
+long aktuellHinkeLengde;
+long naverendeAvstand;
+const int knappPin = 8;
+boolean klarteDetEllerIkke = false;
+int forsteAvstand;
+int andreAvstand;
+
+//# alt for å fikse poeng og motorer
 const int PLAYER1_BUTTON = A4;
 const int PLAYER2_BUTTON = A5;
 const int PLAYER3_BUTTON = A3;
@@ -24,47 +54,7 @@ Servo PLAYER3_SERVO;
 Servo PLAYER4_SERVO;
 
 Servo currentServo;
-//
-
-#define echoPin 9
-#define trigPin 10
-
-const char *utfordringer[] = {"Gjett 20cm avstand", "Skyt"};
-const char *utfordringerLang[] = {"Gjett 20cm avstand",
-                                  "Hink lengre enn venstremann",
-                                  "Skyt eldste spiller på 10 meter",
-                                  "Sta pa henda i 10sek",
-                                  "Dobbelt poeng pa neste utfordring",
-                                  "-1 poeng ved feil pa neste utfordring",
-                                  "Sta 1 meter unna sensor",
-                                  "Skyt i varet og fang skuddet",
-                                  "20 push-ups",
-                                  "30 squats",
-                                  "Gjor 1 pistol squat",
-                                  "Forste til a skyte med hoyrehand",
-                                  "Kast 3 terninger hver med hoyremann, den med høyest samlet sum vinner",
-                                  "Skyt ned en brikke fra 10 meter, sett brikke foran sensor",
-                                  "Skyt spiller til venstre fra 5 meter imens han/hun dukker unna"
-                                 };
-long currentUtfordring;
-const char *aktuellUtfordring;
-long aktuellHinkeLengde;
-long naverendeAvstand;
-const int knappPin = 8;
-boolean klarteDetEllerIkke = false;
-int forsteAvstand;
-int andreAvstand;
-int lengde;
-
-int vinkelVenstre;
-int vinkelMidt = 0;
-int vinkelHoyre;
-int poengVenstre = 0;
-int poengMidt = 0;
-int poengHoyre = 0;
-const char* hoyreNavn = "hoyre";
-const char* midtNavn = "midt";
-const char* venstreNavn = "venstre";
+//#
 
 void setup() {
   pinMode(knappPin, INPUT);
@@ -75,113 +65,40 @@ void setup() {
   pinMode(PLAYER1_BUTTON, INPUT);
   pinMode(PLAYER2_BUTTON, INPUT);
   pinMode(PLAYER3_BUTTON, INPUT);
-  
+
   PLAYER1_SERVO.attach(PLAYER1_SERVO_PIN, 500, 2500);
   PLAYER2_SERVO.attach(PLAYER2_SERVO_PIN, 500, 2500);
   PLAYER3_SERVO.attach(PLAYER3_SERVO_PIN, 500, 2500);
-  
+
   PLAYER1_SERVO.write(0);
   PLAYER2_SERVO.write(0);
   PLAYER3_SERVO.write(0);
   PLAYER4_SERVO.write(0);
   //
 
-  randomSeed(analogRead(0));
+  randomSeed(666);
   Serial.begin(9600);
-  vinkelVenstre = map(poengVenstre, 0, 5, 0, 179);
-  vinkelHoyre = map(poengHoyre, 0, 5, 0, 179);
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
   lcd.print("Velkommen!");
-  delay(2000);
 }
 
 void loop() {
-  // om knapp trykkes, start showet!
+  // om knapp trykkes vis en utfordring
   if (digitalRead(knappPin) == HIGH) {
-    lcd.noAutoscroll();
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Utfordringen:");
-    currentUtfordring = random(sizeof(utfordringerLang) / sizeof(char*));
-    //currentUtfordring = 0;
-    aktuellUtfordring = utfordringerLang[currentUtfordring];
-    skrivLCD(aktuellUtfordring);
-    delay(500);/*
-    lcd.setCursor(0, 1);
-    lcd.print(aktuellUtfordring);
-    int aktuellUtfordringLengde = strlen(aktuellUtfordringLengde);
-    if (aktuellUtfordringLengde > 6) {
-      const char* str = "     ";
-      // hvis setningen er for lang, vil den scrolle
-        lcd.setCursor(11, 0);
-        lcd.autoscroll();
-        for (int i = 0; i < strlen(str); i++)
-        {
-          lcd.print(str[i]);
-          checkPlayerPoeng();
-          delay(400);
-        }
-      }*/
-    Serial.print("CurrentUtfordring, altså nr i arrayen, er: ");
-    Serial.println(currentUtfordring);
-    Serial.print("Og det er utfordringen: ");
-    Serial.println(aktuellUtfordring);
-    delay(3000);
-    /*0"Gjett 20cm avstand", 1"Hink lengre enn venstremann", 2"Skyt eldste spiller på 10 meter",
-      3"Sta på henda i 10sek", 4"Dobbelt poeng på neste utfordring",
-      5"-1 poeng ved feil på neste utfordring", 6"Sta 1 meter unna sensor",
-      7"Skyt i varet og fang skuddet", 8"20 push-ups",
-      9"30 squats", 10"Gjor 1 pistol squat", 11"Forste til a skyte med hoyrehand"*/
-
-    if (aktuellUtfordring == utfordringerLang[0]) {
-      // om tildelt utfordring er 20 cm avstand
-      klarteDetEllerIkke = avstandsGjetter(20);
-      if (klarteDetEllerIkke == false) {
-        skrivLCD("Et forsok til!");
-        avstandsGjetter(20);
-      }
-      skrivLCD("Velg ny utfordring!");
-    }
-    else if (aktuellUtfordring == utfordringerLang[1]) {
-      // hink lengre enn andremann
-      // male til avstand er 140 cmmeter
-      // måle først at de står x cmeter unna, så hoppe og måle hva avstand blir
-      lcd.clear();
-      lcd.print("Forstemann hinker");
-      forsteAvstand = hinkeLengde(140);
-      lcd.clear();
-      lcd.print("Andremann hinker");
-      andreAvstand = hinkeLengde(140);
-      if (forsteAvstand < andreAvstand) {
-        lcd.clear();
-        lcd.print("Forstemann vant!");
-        //faaPoeng(1, 1); lar brukeren klikke på knappen selv
-      }
-      else if (forsteAvstand > andreAvstand) {
-        lcd.clear();
-        lcd.print("Andremann vant!");
-        //faaPoeng(2, 1);
-      }
-      
-    } else if (aktuellUtfordring == utfordringerLang[6]) {
-        // måle 1 m avstand
-        avstandsGjetter(100);
-      }
+ 	visUtfordring();
   }
-  const char* str = "     ";
-      // hvis setningen er for lang, vil den scrolle
-        lcd.setCursor(11, 0);
-        lcd.autoscroll();
-        for (int i = 0; i < strlen(str); i++)
-        {
-          lcd.print(str[i]);
-          checkPlayerPoeng();
-          delay(400);
-        }
-   checkPlayerPoeng();
+  // autoscroll funksjon
+  const char* str = " ";
+  lcd.setCursor(11, 0);
+  lcd.autoscroll();
+  lcd.print(str[0]);
+  delay(400);
+  // skjekker om en spiller har trykket på +poeng knappen
+  checkPlayerPoeng();
 }
 
+// leser inn hvem som trykket på motorknapper og gir den spilleren poeng
 int readButtonPress(int playerButton, int playerPoeng) {
   if (playerButton == PLAYER1_BUTTON) {
     currentServo = PLAYER1_SERVO;
@@ -204,20 +121,18 @@ int readButtonPress(int playerButton, int playerPoeng) {
   return playerPoeng;
 }
 
+// skriver noe i bunn av skjermen
 void skrivLCD(char* text) {
-    lcd.clear();
     lcd.setCursor(0, 1);
     lcd.print(text);
     delay(800);
     int textLengde = strlen(text);
     // hvis setningen er for lang, vil den scrolle
-    if (textLengde > 6) {
-        const char* str2 = text;
-        //const char* str2 = "         "; // "     "
-        //lcd.setCursor(11, 0);
+    if (textLengde > 15) {
+        char* str2 = text;
         lcd.setCursor(0, 1);
         lcd.autoscroll();
-        for (int i = 0; i < strlen(str2) - 5; i++)
+        for (int i = 0; i < textLengde - 10; i++)
         {
           lcd.print(str2[i]);
           checkPlayerPoeng();
@@ -227,6 +142,59 @@ void skrivLCD(char* text) {
       lcd.noAutoscroll();
   }
 
+// skriver utfordringen på lcd & utforer noen spesielle utfordringer
+void visUtfordring() {
+	lcd.noAutoscroll();
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Utfordringen:");
+    currentUtfordringIndex = random(sizeof(utfordringerLang) / sizeof(char*));
+    //currentUtfordringIndex = 0;
+    aktuellUtfordring = utfordringerLang[currentUtfordringIndex];
+    skrivLCD(aktuellUtfordring);
+    Serial.print("currentUtfordringIndex, altså nr i arrayen, er: ");
+    Serial.println(currentUtfordringIndex);
+    Serial.print("Og det er utfordringen: ");
+    Serial.println(aktuellUtfordring);
+
+    if (aktuellUtfordring == utfordringerLang[0]) {
+      // om tildelt utfordring er 20 cm avstand
+      klarteDetEllerIkke = avstandsGjetter(20);
+      if (klarteDetEllerIkke == false) {
+        skrivLCD("Et forsok til!");
+        avstandsGjetter(20);
+      }
+      skrivLCD("Velg ny utfordring!");
+    }
+    else if (aktuellUtfordring == utfordringerLang[1]) {
+      // hink lengre enn andremann
+      // male til avstand er 140 cmmeter
+      // måle først at de står x cmeter unna, så hoppe og måle hva avstand blir
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Forstemann hinker");
+      forsteAvstand = hinkeLengde(140);
+      lcd.clear();
+      lcd.print("Andremann hinker");
+      andreAvstand = hinkeLengde(140);
+      if (forsteAvstand < andreAvstand) {
+        lcd.clear();
+        lcd.print("Forstemann vant!");
+        //faaPoeng(1, 1); lar brukeren klikke på knappen selv
+      }
+      else if (forsteAvstand > andreAvstand) {
+        lcd.clear();
+        lcd.print("Andremann vant!");
+        //faaPoeng(2, 1);
+      }
+
+    } else if (aktuellUtfordring == utfordringerLang[6]) {
+        // Sta 1 meter unna sensor
+        avstandsGjetter(100);
+      }
+}
+
+// sjekker om noen av spillerne klikket på knappen
 void checkPlayerPoeng() {
     if (digitalRead(PLAYER1_BUTTON) != LOW) {
    player1poeng = readButtonPress(PLAYER1_BUTTON, player1poeng);
@@ -242,14 +210,13 @@ void checkPlayerPoeng() {
   }
   }
 
-// måle avstand og sende tilbake avstanden
+// måle avstand og sende tilbake true eller false om avstanden er lik gjetteMal
 boolean avstandsGjetter(long gjetteMal) {
+  lcd.noAutoscroll();
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Maler om:");
   for (int i = 7; i > 0; i--) {
-    lcd.setCursor(0, 1);
-    lcd.print("         ");
     lcd.setCursor(0, 1);
     lcd.print(i);
     lcd.print(" sek");
@@ -282,7 +249,7 @@ boolean avstandsGjetter(long gjetteMal) {
       return false;
     }
     else if (avstand == gjetteMal) {
-      for (int i = 0; i < 10; i ++) {
+      for (int i = 0; i < 5; i ++) {
         lcd.print("Gratulerer!");
         lcd.setCursor(0, 1);
         lcd.print("Akkurat malet!!");
@@ -306,6 +273,7 @@ boolean avstandsGjetter(long gjetteMal) {
   }
 }
 
+// maler avstanden og returnerer den
 long avstandsMaaler() {
   long varighet, avstand;
   digitalWrite(trigPin, LOW);
@@ -326,6 +294,7 @@ long avstandsMaaler() {
 }
 
 int hinkeLengde(int onsketAvstand) {
+  lcd.noAutoscroll();
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Ga ");
@@ -357,8 +326,6 @@ int hinkeLengde(int onsketAvstand) {
   lcd.setCursor(0, 0);
   lcd.print("Maler om:");
   for (int i = 4; i > 0; i--) {
-    lcd.setCursor(0, 1);
-    lcd.print("         ");
     lcd.setCursor(0, 1);
     lcd.print(i);
     lcd.print(" sek");
